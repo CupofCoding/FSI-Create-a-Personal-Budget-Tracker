8856d081d0e0bb97fbc99b91491f7b1146775242
Anne V <anne@cupofcoding.com>
2021-11-22 20:14:11 -0800

    changes following Personal Budget Tracker (1.10.4)-2

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
+for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
+    let weeklyanswer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses += weeklyanswer
+}
+
+for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
+    let monthlyanswer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses += monthlyanswer
+}
+
+for (let i = 0; i < annualExpenseQuestions.length; i++) {
+    let annualanswer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses += annualanswer
+}
+// console.log(answer)
 // Your Code Here
